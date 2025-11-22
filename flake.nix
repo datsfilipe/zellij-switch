db563e0bc1a793fcdae9bd2644e8f7e1ed937fbd
Filipe Lima <datsfilipe.foss@proton.me>
2025-11-22 03:16:20 -0300

    fix: update stuff and fix build

--- flake.nix
+++ flake.nix
@@ -16,35 +16,55 @@
             inherit system;
             overlays = [ rust-overlay.overlays.default ];
           };
+
           rustToolchain = pkgs.rust-bin.stable.latest.default.override {
             targets = [ "wasm32-wasip1" ];
           };
         in
         {
-          default = pkgs.rustPlatform.buildRustPackage {
+          default = pkgs.rustPlatform.buildRustPackage rec {
             pname = "zellij-switch";
             version = "0.2.0";
+            cargoLock = { lockFile = ./Cargo.lock; };
             src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-            
-            nativeBuildInputs = [ rustToolchain ];
+
+            nativeBuildInputs = [
+              rustToolchain
+              pkgs.pkg-config
+              pkgs.perl
+            ];
+
+            buildInputs = [
+              pkgs.zlib
+            ];
+
+            cargo = "${rustToolchain}/bin/cargo";
+            rustc = "${rustToolchain}/bin/rustc";
+
+            cargoBuildFlags = [
+              "--target" "wasm32-wasip1"
+              "--release"
+            ];
 
             buildPhase = ''
-              cargo build --target wasm32-wasip1 --release
+              "${cargo}" build ${pkgs.lib.concatStringsSep " " cargoBuildFlags}
             '';
 
             installPhase = ''
               mkdir -p $out/bin
-              cp target/wasm32-wasip1/release/*.wasm $out/bin/
+              cp target/wasm32-wasip1/release/*.wasm $out/bin/ || true
             '';
 
             doCheck = false;
+
+            meta = with pkgs.lib; {
+              description = "zellij-switch (wasm build only)";
+              license = licenses.mit;
+            };
           };
         });
 
-      defaultPackage = eachSystem (system:
-        self.packages.${system}.default);
-
+      defaultPackage = eachSystem (system: self.packages.${system}.default);
       overlays.default = final: prev: {
         zellij-switch = self.packages.${prev.system}.default;
       };
